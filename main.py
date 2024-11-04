09a9afc8ce2dc51da5f91c821f0c82117ac023c3
RasperRevision <131694670+RasperRevision@users.noreply.github.com>
2024-11-04 10:03:45 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+# 1
+list = [1, 2, 3, 4, 5]
+
+# 2
+def sum_of_list(l):
+    total = 0
+    for i in l:
+        total += i
+    return total
+
+# 3
+my_list = ["Gouda", "Stilton", "Gorgonzola", "Cheddar"]
+print(my_list[-1])
+
+# 4
+fruits = ["apple", "banana", "cherry"]
+fruits.append("orange")
+
+# 5
+fruits.remove("banana")
+print(fruits)
