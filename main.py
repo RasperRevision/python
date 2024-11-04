6526f73e2024120a6017de255a9a3a85992467c7
RasperRevision <131694670+RasperRevision@users.noreply.github.com>
2024-11-04 10:08:17 +0000

    commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # 1
 list = [1, 2, 3, 4, 5]
+print(list)
 
 # 2
 def sum_of_list(l):
@@ -7,6 +8,7 @@ def sum_of_list(l):
     for i in l:
         total += i
     return total
+print(sum_of_list(list))
 
 # 3
 my_list = ["Gouda", "Stilton", "Gorgonzola", "Cheddar"]
@@ -15,6 +17,7 @@ print(my_list[-1])
 # 4
 fruits = ["apple", "banana", "cherry"]
 fruits.append("orange")
+print(fruits)
 
 # 5
 fruits.remove("banana")
