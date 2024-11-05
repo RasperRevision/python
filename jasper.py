b04cf495a1fedf50bc2d714810446ef1e8abbbec
Karl-MrX <20StocksJ@roundwoodpark.co.uk>
2024-11-05 09:07:28 +0000

    Update jasper.py

--- jasper.py
+++ jasper.py
@@ -12,3 +12,20 @@ fruits.remove("banana")
 #the thing to delete based on name, how to go backward on array need to be figured out
 print(fruits)
 print(myList)
+
+#a function in python isa subroutine that returns something, shortens repeated code.
+name=input("what is name")
+print("Hello ", name)
+def product(num1,num2):
+    num3=num1*num2
+    return num3
+# you return a value like this:
+product=product(3,4)
+def evenorodd(number):
+    if float(number)/2 == int(float(number)/2):
+        answer=True
+    else:
+        answer=False
+    return answer
+num4=int(input("number:"))
+print(evenorodd(num4))
