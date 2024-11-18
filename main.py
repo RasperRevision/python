a4f2204dbfb42cad567642aa1cd102014f33b72c
RasperRevision <131694670+RasperRevision@users.noreply.github.com>
2024-11-18 10:01:35 +0000

    modified:   main.py

--- main.py
+++ main.py
@@ -1,39 +1,11 @@
-# 1
-list = [1, 2, 3, 4, 5]
-print(list)
+string = "Hello" + "World"
 
-# 2
-def sum_of_list(l):
-    total = 0
-    for i in l:
-        total += i
-    return total
-print(sum_of_list(list))
+print("I love " + "Python")
 
-# 3
-my_list = ["Gouda", "Stilton", "Gorgonzola", "Cheddar"]
-print(my_list[-1])
+string = "My favourite number is " + str(-201)
 
-# 4
-fruits = ["apple", "banana", "cherry"]
-fruits.append("orange")
-print(fruits)
+first_name = "John"
+last_name = "Doe"
+full_name = first_name + " " + last_name
 
-# 5
-fruits.remove("banana")
-print(fruits)
-
-# 1
-# A function is a type of subprogram that returns an output, usually when given input parameters. It is useful to avoid repetition
-
-# 2
-def greet(name):
-    print(f'Hello, {name}')
-
-# 3
-def product(a, b):
-    return a * b
-
-# 4
-def is_even(n):
-    return n % 2 == 0
+# Concatenation places the values next to each other, whereas addition returns the sum of the numerical values
